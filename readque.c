f614be21d2a340fd69d9ec80266b9ef9aa1b870d
shradhayewale <yewalesa15.it@coep.ac.in>
2015-10-23 23:00:56 +0530

    Update readque.c

--- readque.c
+++ readque.c
@@ -2,25 +2,28 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<errno.h>
-#include"list.h"
+#include"quiz.h"
 int main(int argc,char *argv[]) {
 	
-	bank b1;
-	int fd = open(argv[1], O_RDONLY);
-	double index=atoi(argv[2]);
-	if(fd == -1) {
-		perror("open failed:");
-		return errno;
-	}
-
-	while(read(fd, &b1, sizeof(bank))) {
-		if(index == b1.index) {
-		printf("%lf \n", b1.index);
-		printf("%s\n",b1.que);
-		printf("%d\n",b1.ans);
-		return 0;
+	bank b;
+	int index1, pt=0;
+	printf("enter index");
+	scanf("%d", &index1);
+	FILE *fp = fopen("file5.txt", "r");
+	
+	
+	while(fread(&b, sizeof(bank), 1, fp)) {
+		if(index1 == b.index) {
+			printf("%d\n",b.index);
+			printf("%s\n",b.que);
+			printf("%c\n",b.ans);
+			
                 }
+		else{
+			printf("Record not found");
+		} 
+			
         }
-	close(fd);
+	fclose(fp);
 	return 0;
 }		
