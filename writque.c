a4a6ed158385c7657c6f23fb2e26edc1e36b604e
shradhayewale <yewalesa15.it@coep.ac.in>
2015-10-23 23:02:18 +0530

    Update writque.c

--- writque.c
+++ writque.c
@@ -1,21 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<fcntl.h>
+#include<stdio.h>
 #include<errno.h>
-#include"list.h"
+#include"quiz.h"
+
 int main(int argc,char *argv[]) {
 	
 	
-	bank b1;
-	int fd = open(argv[1], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-	if(fd == -1) {
-		perror("open failed:");
-		return errno;
+	bank b;
+	char val;
+	int i=0;
+	FILE *fp = fopen("file1.txt","a");
+
+	printf("Enter index\n");
+	scanf("%d",&b.index);
+
+	printf("enter que n option\n");
+	val=getchar();
+	while(((val=getchar()) != '\n') && (val != EOF)) {
+		b.que[i]=val;
+		i++;
+		
 	}
-	/* scanf returns -1 when user presses ctrl-d. EOF is #defined to be -1 in stdio.h */
-	while(scanf("%lf %s %d", &b1.index,b1.que,&b1.ans) != EOF)
-		write(fd, &b1, sizeof(bank));
-	close(fd);
+	b.que[i]='\0';	 
+	
+	printf("Enter ans:\n");
+	scanf("%c",&b.ans);
+
+	fwrite(&b,sizeof(bank),1,fp);
+	fclose(fp);
 	return 0;
 }
 	
