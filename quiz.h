4a1ed877641950135c02eb0fde27454e62b340b6
shradhayewale <yewalesa15.it@coep.ac.in>
2015-10-25 22:00:20 +0530

    Update quiz.h

--- quiz.h
+++ quiz.h
@@ -1,13 +1,17 @@
 typedef struct bank {
 	int index;
 	char que[1024];
-   	char ans;
+   	int ans;
 }bank;
 typedef struct account {
 	char name[20];
 	int score;
 }account;		
 void help();
-void write_score(int a , char name[20]);
+void write_score(int a , char name[20],char fname[10]);
 void display_score();
-
+int checking(int ans,int mainans);
+void addque(char fname[20]);
+void add_new_passwd(char newpasswd[20]);
+int readindex(char fname[20]);
+void writeindex(char fname[20],int cnt);
